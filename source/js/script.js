949e8e4382cb76f34d774148d4deecc7f69bcfcd
Edwin18 <igreenbergi78@gmail.com>
2019-08-04 02:10:09 +0300

    исправил карту сделал слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -13,49 +13,59 @@ button.addEventListener("click", function(evn) {
 });
 
 // SLIDER - BEFORE AFTER
+if (document.querySelector(".example__button")) {
+  var btnBefore = document.querySelector(".example__button--before");
+  var btnAfter = document.querySelector(".example__button--after");
+  var imageAfter = document.querySelector(".example__image-after");
+  var imageBar = document.querySelector(".example__controls-bar");
+  var mediaTablet = window.matchMedia("(min-width: 768px)");
 
-if (document.querySelector("#example-cat")) {
-  function range() {
-    var range = document.querySelector("#example-cat");
-    var imageAfter = document.querySelector(".example__image-after");
-    imageAfter.style.width = range.value +  "%";
+  btnBefore.addEventListener("click", function(evn) {
+    evn.preventDefault();
+    imageAfter.style.width = "0";
+    imageBar.classList.remove("example__controls-bar--after");
+  });
+
+  btnAfter.addEventListener("click", function(evn) {
+    evn.preventDefault();
+    imageAfter.style.width = "100%";
+    imageBar.classList.add("example__controls-bar--after");
+  });
+
+  if (mediaTablet) {
+    var btnBar = document.querySelector(".example__controls-button");
+
+    imageAfter.style.width = parseInt(window.getComputedStyle(btnBar).left, 10) / 4 + "%";
+
+    btnBar.onmousedown = function(e) {
+      var startOffset = e.pageX;
+      var oldOffset = parseInt(window.getComputedStyle(btnBar).left, 10);
+      function moveAt(e) {
+        newOffset = startOffset - e.pageX;
+        if (newOffset > 0) {
+          var widthRight = oldOffset - newOffset;
+          btnBar.style.left = widthRight + "px";
+          imageAfter.style.width = widthRight / 4 + "%";
+          if (parseInt(btnBar.style.left, 10) <= 0) {
+            btnBar.style.left = 0;
+          }
+        }
+        if (newOffset < 0) {
+          var widthLeft = oldOffset + Math.abs(newOffset);
+          btnBar.style.left = widthLeft + "px";
+          imageAfter.style.width = widthLeft / 4 + "%";
+          if (parseInt(btnBar.style.left, 10) >= 394) {
+            btnBar.style.left = "394px";
+          }
+        }
+      }
+      document.onmousemove = function(e) {
+        moveAt(e);
+      }
+      document.onmouseup = function() {
+        document.onmousemove = null;
+        btnBar.onmouseup = null;
+      }
+    };
   }
-
-  range();
 }
-
-var btnBefore = document.querySelector(".example__button--before");
-var btnAfter = document.querySelector(".example__button--after");
-var imageAfter = document.querySelector(".example__image-after");
-var imageBar = document.querySelector(".example__controls-bar");
-
-btnBefore.addEventListener("click", function(evn) {
-  evn.preventDefault();
-  imageAfter.style.width = "0";
-  imageBar.classList.remove("example__controls-bar--after");
-});
-
-btnAfter.addEventListener("click", function(evn) {
-  evn.preventDefault();
-  imageAfter.style.width = "100%";
-  imageBar.classList.add("example__controls-bar--after");
-});
-
-// if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-//   jQuery('.art-img img').each(function(){
-//       var t = jQuery(this),
-//           s = 'url(' + t.attr('src') + ')',
-//           p = t.parent(),
-//           d = jQuery('<div></div>');
-
-//       p.append(d);
-//       d.css({
-//           'height'                : t.parent().css('height'),
-//           'background-size'       : 'cover',
-//           'background-repeat'     : 'no-repeat',
-//           'background-position'   : '50% 20%',
-//           'background-image'      : s
-//       });
-//       t.hide();
-//   });
-// }
